3aec425a39180fba0315e30990f79a4ab220ac50
Pravin Kumar <prkumartech@gmail.com>
2017-07-27 21:24:33 -0500

    Fixed CORE issue and Updated Json data

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require('express');
 var data = require('./superhero.json')
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
     console.log("Got a GET request for the homepage");
     res.send(data);
